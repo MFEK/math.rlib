017c4d585ed9c61a65c3c7a79eeb20ec9f1da97c
Fredrick Brennan <copypaste@kittens.ph>
2021-11-07 07:52:03 -0500

    [Piecewise::to_contour] Write open path's last point
    Solves round-trip issue I just discovered while writing `MFEKpathops`
    initial version.

--- src/piecewise/glif.rs
+++ src/piecewise/glif.rs
@@ -104,16 +104,14 @@ impl Piecewise<Bezier> {
         let mut last_curve: Option<[Vector; 4]> = None;
 
         let mut first_point = true;
-        for curve in &self.segs
-        {                       
+        for curve in &self.segs {
             let control_points = curve.to_control_points();
 
             let point_type = if first_point && !self.is_closed() { PointType::Move } else { PointType::Curve };
             let mut new_point = control_points[0].to_point(control_points[1].to_handle(), Handle::Colocated, point_type);
 
             // if this isn't the first point we need to backtrack and set our output point's b handle
-            match last_curve
-            {
+            match last_curve {
                 Some(lc) => {
                     new_point.b = lc[2].to_handle();
                 }
@@ -126,14 +124,19 @@ impl Piecewise<Bezier> {
             first_point = false;
         }
 
+        if output_contour.len() <= 1 { return output_contour }
 
-        if output_contour.len() > 1 && self.is_closed() {
+        if self.is_closed() {
             let fp = output_contour.first_mut().unwrap();
 
             // we've got to connect the last point and the first point
-             fp.b = Vector::to_handle(last_curve.unwrap()[2]);
+            fp.b = Vector::to_handle(last_curve.unwrap()[2]);
+        } else {
+            let control_points = last_curve.unwrap();
+            let new_point = control_points[3].to_point(control_points[2].to_handle(), Handle::Colocated, PointType::Curve);
+            output_contour.push(new_point);
         }
     
-        return output_contour;
+        output_contour
     }
 }
