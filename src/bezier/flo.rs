b33e2d3407e371964810581a6a5c7ca9add44b1e
Fredrick Brennan <copypaste@kittens.ph>
2021-11-23 13:42:44 -0500

    implement `BezierPath(Factory)` for `Piecewise<Bezier>`

--- src/bezier/flo.rs
+++ src/bezier/flo.rs
@@ -1,5 +1,7 @@
 use super::super::Vector;
-use flo_curves::bezier::{BezierCurve, BezierCurveFactory};
+use crate::Piecewise;
+
+use flo_curves::bezier::{BezierCurve, BezierCurveFactory, path::{BezierPath, BezierPathFactory}};
 use flo_curves::geo::Geo;
 
 
@@ -31,5 +33,32 @@ impl BezierCurve for Bezier {
     {
         let cp = self.to_control_points();
         (cp[1], cp[2])
+    }
+}
+
+impl Geo for Piecewise<Bezier> {
+    type Point = Vector;
+}
+
+impl BezierPath for Piecewise<Bezier> {
+    type PointIter = std::vec::IntoIter<(Self::Point, Self::Point, Self::Point)>;
+
+    fn start_point(&self) -> Self::Point {
+        self.segs[0].to_control_points()[0]
+    }
+
+    fn points(&self) -> Self::PointIter {
+        self.segs.iter().map(|s|(s.to_control_points()[1], s.to_control_points()[2], s.to_control_points()[3])).collect::<Vec<_>>().into_iter()
+    }
+}
+
+impl BezierPathFactory for Piecewise<Bezier> {
+    fn from_points<FromIter: IntoIterator<Item=(Self::Point, Self::Point, Self::Point)>>(mut start_point: Self::Point, points: FromIter) -> Self {
+        let mut vb: Vec<Bezier> = vec![];
+        for p in points.into_iter() {
+            vb.push(Bezier::from_points(start_point, p.0, p.1, p.2));
+            start_point = p.2;
+        }
+        Piecewise::new(vb, None)
     }
 }
