a5e6b85ed421cfc2e16cee3aacaf0a74cf8ae42e
Fredrick Brennan <copypaste@kittens.ph>
2021-12-27 20:38:32 -0500

    [fixup] decrease default colocation assertion `within`

--- src/fixup.rs
+++ src/fixup.rs
@@ -1,15 +1,13 @@
 use glifparser::{Outline, PointData};
 
 pub trait Fixup {
-    fn assert_colocated(&mut self);
+    fn assert_colocated(&mut self) {
+        self.assert_colocated_within(0.0000001)
+    }
     fn assert_colocated_within(&mut self, within: f32);
 }
 
 impl<PD: PointData> Fixup for Outline<PD> {
-    fn assert_colocated(&mut self) {
-        self.assert_colocated_within(1.);
-    }
-
     fn assert_colocated_within(&mut self, within: f32) {
         for c in self.iter_mut() {
             for p in c.iter_mut() {
