29c3c4a08b7d606440d920e9c34d2bcf0370770a
T Prajwal Prabhu <66326051+prajwalprabhu@users.noreply.github.com>
2021-12-31 13:49:03 +0530

    +`fit_to_points` module
    * It returns the control point if you give it knot(end) points
    
    implimented mixture of contour
    
    * changed the module name
    
    * changed the type
    
    In fit_to_points/get_control_points.rs canged the type from `Point` to `(f32,f32)`
    
    * rustfmt whole project

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,42 @@
 #![allow(non_snake_case)] // for our name MFEKmath
-
-pub mod vector;
-pub mod piecewise;
-pub mod rect;
-pub mod bezier;
 pub mod arclenparameterization;
+pub mod bezier;
 pub mod consts;
+pub mod coordinate;
+#[cfg(feature = "default")]
+pub mod dash_along_path;
 pub mod evaluate;
+pub mod fit_to_points;
 pub(crate) mod fixup;
+pub mod glyphbuilder;
+pub mod interpolator;
 pub mod parameterization;
+#[cfg(feature = "default")]
+pub mod pattern_along_path;
+pub mod piecewise;
 pub mod polar;
 pub mod primitive;
-pub mod coordinate;
-pub mod interpolator;
-pub mod glyphbuilder;
-#[cfg(feature="default")]
+pub mod rect;
+#[cfg(feature = "default")]
 pub mod variable_width_stroking;
-#[cfg(feature="default")]
-pub mod pattern_along_path;
-#[cfg(feature="default")]
-pub mod dash_along_path;
-#[cfg(feature="default")]
-pub use {skia_safe, self::{variable_width_stroking::*, pattern_along_path::*, dash_along_path::*}};
+pub mod vector;
 
-pub use self::vector::Vector;
-pub use self::piecewise::Piecewise;
-pub use self::rect::Rect;
-pub use self::bezier::Bezier;
-pub use self::parameterization::Parameterization;
+#[cfg(feature = "default")]
+pub use {
+    self::{dash_along_path::*, pattern_along_path::*, variable_width_stroking::*},
+    skia_safe,
+};
+
 pub use self::arclenparameterization::ArcLengthParameterization;
+pub use self::bezier::Bezier;
 pub use self::glyphbuilder::GlyphBuilder;
+pub use self::parameterization::Parameterization;
+pub use self::piecewise::Piecewise;
 pub use self::primitive::Primitive;
+pub use self::rect::Rect;
+pub use self::vector::Vector;
 
 pub use self::evaluate::Evaluate;
-pub use self::evaluate::{EvalScale, EvalRotate, EvalTranslate};
+pub use self::evaluate::{EvalRotate, EvalScale, EvalTranslate};
 pub use self::fixup::Fixup;
-pub use self::interpolator::{Interpolator, InterpolationType};
+pub use self::interpolator::{InterpolationType, Interpolator};
